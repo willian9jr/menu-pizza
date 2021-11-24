74e2206ea3225c7394f7d0f8690f8334c1612f67
Willian <willian9jr@hotmail>
2021-11-24 20:46:30 -0300

    finalizando kart desktop

--- script.js
+++ script.js
@@ -97,7 +97,80 @@ c(".pizzaInfo--addButton").addEventListener('click', ()=>{
         });
     }
 
-    
-
+    updateCart();
     closeModal();
 });
+
+c('.menu-openner').addEventListener('click', ()=>{
+    if(cart.length > 0){
+        c('aside').style.left = '0px'
+    }else{
+        c('aside').style.left = '100px'
+    }
+    
+})
+
+function updateCart(){
+    c('.menu-openner span').innerHTML = cart.length; 
+
+    if(cart.length > 0){
+        c("aside").classList.add('show');
+        c(".cart").innerHTML = ""
+
+        let subTotal = 0;
+        let desconto = 0;
+        let total = 0;
+
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item)=> item.id == cart[i].id);
+            subTotal += pizzaItem.price * cart[i].qt;
+
+
+            let cartItem = c('.models .cart--item').cloneNode(true);
+            
+            let pizzaSizeName;
+            switch(cart[i].size){
+                case 0:
+                    pizzaSizeName = "P";
+                    break;
+                case 1:
+                    pizzaSizeName = "M";
+                    break;
+                case 2:
+                    pizzaSizeName = "G";
+                    break;
+            }
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML= cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () =>{
+                if(cart[i].qt > 1){
+                    cart[i].qt--;
+                    
+                }else{
+                    cart.splice(i,1);
+                }
+                updateCart();
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () =>{
+                cart[i].qt++;
+                updateCart();
+            })
+
+            c('.cart').append(cartItem);
+        }
+        
+        desconto = subTotal * 0.1;
+        total = subTotal - desconto;
+
+        c(".subtotal span:last-child").innerHTML = `R$ ${subTotal.toFixed(2)}`;
+        c(".desconto span:last-child").innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c(".total span:last-child").innerHTML = `R$ ${total.toFixed(2)}`;
+        
+    }else{
+        c("aside").classList.remove('show');
+    }
+}
+
